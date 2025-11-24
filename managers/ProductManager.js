cea602ad57fe8af71ec89325c8d45de3df6e5697
CarrizoMateo <carrizomateodw@gmail.com>
2025-11-24 20:11:09 -0300

    segunda entrega

--- managers/ProductManager.js
+++ managers/ProductManager.js
@@ -10,14 +10,15 @@ export default class ProductManager {
     return JSON.parse(data);
   }
 
-  async getProductById(id) {
-    const products = await this.getProducts();
-    return products.find(p => p.id == id);
+  async saveProducts(products) {
+    await fs.writeFile(this.path, JSON.stringify(products, null, 2));
   }
 
   async addProduct(productData) {
     const products = await this.getProducts();
-    const newId = products.length > 0 ? (parseInt(products[products.length - 1].id) + 1).toString() : "1";
+    const newId = products.length > 0
+      ? (parseInt(products[products.length - 1].id) + 1).toString()
+      : "1";
 
     const newProduct = {
       id: newId,
@@ -25,20 +26,8 @@ export default class ProductManager {
     };
 
     products.push(newProduct);
-    await fs.writeFile(this.path, JSON.stringify(products, null, 2));
+    await this.saveProducts(products);
     return newProduct;
-  }
-
-  async updateProduct(id, updateData) {
-    const products = await this.getProducts();
-    const index = products.findIndex(p => p.id == id);
-    if (index === -1) return null;
-
-    if (updateData.id) delete updateData.id;
-
-    products[index] = { ...products[index], ...updateData };
-    await fs.writeFile(this.path, JSON.stringify(products, null, 2));
-    return products[index];
   }
 
   async deleteProduct(id) {
@@ -47,9 +36,10 @@ export default class ProductManager {
     if (index === -1) return false;
 
     products.splice(index, 1);
-    await fs.writeFile(this.path, JSON.stringify(products, null, 2));
+    await this.saveProducts(products);
     return true;
   }
 }
+
 
 
---
+++ src/app.js
@@ -0,0 +1,74 @@
+// Paso 1 y 2: Handlebars y ruta /home
+// npm install express express-handlebars socket.io
+
+import express from 'express';
+import { engine } from 'express-handlebars';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Server } from 'socket.io';
+import http from 'http';
+import ProductManager from './managers/ProductManager.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const productManager = new ProductManager('./data/products.json');
+
+const PORT = 8080;
+
+// Config handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, '/views'));
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Ruta home
+app.get('/home', async (req, res) => {
+  const products = await productManager.getProducts();
+  res.render('home', {
+    title: 'Lista de Productos',
+    products,
+  });
+});
+
+// Ruta realtimeproducts
+app.get('/realtimeproducts', async (req, res) => {
+  const products = await productManager.getProducts();
+  res.render('realTimeProducts', { products });
+});
+
+// Socket.io
+io.on('connection', async (socket) => {
+  console.log('Cliente conectado');
+
+  // Emitir productos actuales al conectar
+  const products = await productManager.getProducts();
+  socket.emit('productList', products);
+
+  // Escuchar nuevo producto
+  socket.on('addProduct', async (data) => {
+    await productManager.addProduct(data);
+    const updatedProducts = await productManager.getProducts();
+    io.emit('productList', updatedProducts);
+  });
+
+  // Escuchar eliminar producto
+  socket.on('deleteProduct', async (id) => {
+    await productManager.deleteProduct(id);
+    const updatedProducts = await productManager.getProducts();
+    io.emit('productList', updatedProducts);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
+
